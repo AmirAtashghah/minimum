1004f7f4167825a2a466697ef00b39f3df9ea11d
majidgholami2000 <majid1378.gholami@gmail.com>
2024-01-18 13:14:50 +0330

    add user app

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('blog/<str:pk>/', views.singleBlog, name='singleBlog'),
+  path('login/', views.user_login, name='login'),
+  path('signup/', views.user_signup, name='signup'),
+  path('logout/', views.user_logout, name='logout'),
+  path('create/', views.create_blog_post, name='create_blog_post'),
+  ]
