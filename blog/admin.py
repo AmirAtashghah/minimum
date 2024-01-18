da193b6027185225ac86fdcd2e875acc96d5b81c
AmirAtashghah <125047918+AmirAtashghah@users.noreply.github.com>
2024-01-18 12:46:18 +0330

    Delete blog directory

--- blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile,BlogPost, Comment
-
-admin.site.register(UserProfile)
-admin.site.register(BlogPost)
-admin.site.register(Comment)
-
